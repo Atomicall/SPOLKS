d98f0865fca2c56827cb889f2caa3e9f6e95f226
DmitryZarubo <dmitri2002zarubo@mail.ru>
2022-10-03 01:51:26 +0300

    added TODOs and started server development

--- Laba1/server/main.go
+++ Laba1/server/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
+
+	sendfile "github.com/Atomicall/SPOLKS/Laba1/server/packages/sendFile"
 )
 
 func main() {
-	fmt.Print("It is server!\n")
+	server, err := net.Listen("tcp", "localhost:27001")
+	if err != nil {
+		fmt.Println("Error listetning: ", err)
+		os.Exit(1)
+	}
+	defer server.Close()
+
+	fmt.Println("Server started! Waiting for connections...")
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+		fmt.Println("Client connected")
+		sendfile.SendFileToClient(connection)
+	}
 }
